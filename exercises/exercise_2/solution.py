cf16f817fb0f280102160a3811b277fbebae7b54
Kenyu Gwe <113727083+abimmost@users.noreply.github.com>
2025-09-05 23:21:49 +0100

    Implement streaming response functionality in solution.py for exercise 2

--- exercises/exercise_2/solution.py
+++ exercises/exercise_2/solution.py
@@ -2,13 +2,14 @@ from config import connect_to_llm
 import time
 
 llm = connect_to_llm()
-prompt = "Why do trees go barren?"
+while True:
+    prompt = input("\nEnter your prompt: ")
 
-response = llm.invoke(prompt)
-print("Response from invoke:\n", response)
+    # response = llm.invoke(prompt)
+    # print("Response from invoke:\n", response)
 
-time.sleep(4)
+    # time.sleep(4)
 
-print("\n\nResponse from stream:")
-for part in llm.stream(prompt):
-    print(part, end="", flush=True)
+    print("\nResponse from stream:")
+    for part in llm.stream(prompt):
+        print(part, end="", flush=True)
