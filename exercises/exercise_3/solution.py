b9cb59fd79b6e1475f0de86ae29a1ffe0bc365d7
Kenyu Gwe <113727083+abimmost@users.noreply.github.com>
2025-09-13 10:37:43 +0100

    Refactor LLM connection to use load_google_chat_model function in solution.py

--- exercises/exercise_3/solution.py
+++ exercises/exercise_3/solution.py
@@ -1,6 +1,6 @@
-from config import connect_to_llm_chat
+from configs import load_google_chat_model
 
-llm_chat = connect_to_llm_chat()
+llm_chat = load_google_chat_model()
 messages = [
     ("system", "You are a helpful assistant specialized in human anatomy"),
     ("user", "Can digestive acid in my stomach be secreted even without food in it?")
