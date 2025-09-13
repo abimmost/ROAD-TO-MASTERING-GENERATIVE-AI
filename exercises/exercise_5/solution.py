c3f70dc4e02337f44a4b4d75cd2625e96cfae81b
Kenyu Gwe <113727083+abimmost@users.noreply.github.com>
2025-09-13 10:33:48 +0100

    Implement chat functionality in solution.py for Exercise 5

--- exercises/exercise_5/solution.py
+++ exercises/exercise_5/solution.py
@@ -1,1 +1,14 @@
- 
+from configs  import load_google_chat_model
+
+chat = load_google_chat_model
+
+print("THE AI ASSISTANT WELCOMES YOU")
+print("-"*30,"\n Begin by entering a prompt")
+
+while True:
+    prompt = input("USER: ")
+    if prompt.lower() == "quit" or "exit":
+        print("Goodbyte...")
+        break
+    for part in chat.stream(prompt):
+        print(part, end="", flush=True)
